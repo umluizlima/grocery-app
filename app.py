0c4506c245a14685b715a7abb0351e0a6598f74d
umluizlima <umluizlima@gmail.com>
2018-04-19 10:40:43 -0300

    Database and migration

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
+import os
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+# Configs
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# DB
+db = SQLAlchemy(app)
+# DB Migration
+migrate = Migrate(app, db)
 
+# Models
+class Item(db.Model):
+    """
+    CREATE: db.session.add(item) -> db.session.commit()
+    READ: Item.query.all() or Item.query.filter_by(key=value).first()
+    UPDATE: item = Item.query.filter_by(key=value).first() -> item.key = value -> db.session.commit()
+    DELETE: db.session.delete(item) -> db.session.commit()
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Item {self.id}>"
+
+# Views
 @app.route('/')
 def index():
     return render_template('index.html')
