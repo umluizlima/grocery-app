a021beed15dd6a0c13d30109c3c6cf07efd97d43
umluizlima <umluizlima@gmail.com>
2018-04-19 12:47:43 -0300

    Serializing and GETs

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from operator import itemgetter
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 # Configs
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or \
+        'you-will-never-guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # DB
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 # DB Migration
 migrate = Migrate(app, db)
+
 
 # Models
 class Item(db.Model):
     """
     CREATE: db.session.add(item) -> db.session.commit()
     READ: Item.query.all() or Item.query.filter_by(key=value).first()
-    UPDATE: item = Item.query.filter_by(key=value).first() -> item.key = value -> db.session.commit()
+    UPDATE: item = Item.query.filter_by(key=value).first() -> item.key = value\
+    -> db.session.commit()
     DELETE: db.session.delete(item) -> db.session.commit()
     """
     id = db.Column(db.Integer, primary_key=True)
@@ -31,26 +37,46 @@ class Item(db.Model):
     def __repr__(self):
         return f"<Item {self.id}>"
 
+
+class ItemSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'content', 'done')
+
+
 # Views
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/list', methods=["GET", "POST"])
-def list():
-    if request.method == "POST":
-        return "POST on /list"
-    else:
-        return "GET on /list"
 
-@app.route('/list/<id>', methods=["GET", "PUT", "DELETE"])
-def list_id(id):
-    if request.method == "PUT":
+@app.route('/list', methods=["GET"])
+def read_items():
+    items = ItemSchema(many=True).dump(Item.query.all()).data
+    return jsonify(sorted(items, key=itemgetter('done')))
+
+
+@app.route('/list', methods=["POST"])
+def create_item():
+    return "POST on /list"
+
+
+@app.route('/list/<int:id>', methods=["GET"])
+def read_item(id):
+    item = Item.query.filter_by(id=id).first()
+    if not item:
+        return abort(404)
+    return ItemSchema().jsonify(item)
+
+
+@app.route('/list/<id>', methods=["PUT"])
+def update_item(id):
         return f"PUT on /list/{id}"
-    elif request.method == "DELETE":
+
+
+@app.route('/list/<id>', methods=["DELETE"])
+def delete_item(id):
         return f"DELETE on /list/{id}"
-    else:
-        return f"GET on /list/{id}"
+
 
 if __name__ == '__main__':
     app.run(host='localhost',
