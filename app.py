9fcacf51690ef55084894e7bbe4d7b8e0977ba9c
umluizlima <umluizlima@gmail.com>
2018-04-18 23:08:00 -0300

    Draft of the API

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/list', methods=["GET", "POST"])
+def list():
+    if request.method == "POST":
+        return "POST on /list"
+    else:
+        return "GET on /list"
+
+@app.route('/list/<id>', methods=["GET", "PUT", "DELETE"])
+def list_id(id):
+    if request.method == "PUT":
+        return f"PUT on /list/{id}"
+    elif request.method == "DELETE":
+        return f"DELETE on /list/{id}"
+    else:
+        return f"GET on /list/{id}"
 
 if __name__ == '__main__':
     app.run(host='localhost',
